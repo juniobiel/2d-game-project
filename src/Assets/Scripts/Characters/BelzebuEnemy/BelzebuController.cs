5f9974b78e8b6d141b89daf346d6b50e0e83a423
Gabriel Junio <gabriel.junio@rocketmail.com>
2023-09-14 02:23:33 -0300

    Animation integration eight directions working

--- src/Assets/Scripts/Characters/BelzebuEnemy/BelzebuController.cs
+++ src/Assets/Scripts/Characters/BelzebuEnemy/BelzebuController.cs
@@ -3,16 +3,28 @@ using UnityEngine;
 public class BelzebuController : MonoBehaviour
 {
     private Transform _playerTransform;
+    private Animator _animator;
 
     [SerializeField]
     private float Speed;
     private bool CanMove;
 
-    private Vector2 auxActualPosition;
+    private const float Y_UP = 0.95f;
+    private const float Y_DOWN = -0.95f;
+
+    private const float Y_UP_TOLERANCE = 0.03f;
+    private const float Y_DOWN_TOLERANCE = - 0.3f;
+
+    private const float X_RIGHT_TOLERANCE = 0.3f;
+    private const float X_LEFT_TOLERANCE = - 0.3f;
+
+    private const float X_RIGHT = 0.95f;
+    private const float X_LEFT = - 0.95f;
 
     private void Start()
     {
         _playerTransform = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        _animator = gameObject.GetComponent<Animator>();
         CanMove = true;
     }
 
@@ -21,28 +33,21 @@ public class BelzebuController : MonoBehaviour
     {
         var playerDistance = Vector2.Distance(_playerTransform.position, gameObject.transform.position);
 
-        if(playerDistance > 1.0f && !CanMove) 
+        if(playerDistance > 2.0f && !CanMove) 
             CanMove = true;
         
-
         if(CanMove)
         {
-            auxActualPosition = transform.position;
+            var heading = _playerTransform.position - transform.position;
+            var direction = heading / heading.magnitude;
+
             var moveTowards = Vector2.MoveTowards(transform.position, _playerTransform.position, Speed * Time.deltaTime);
 
             transform.position = moveTowards;
 
-            float positionX = auxActualPosition.x - transform.position.x;
-            float positionY = auxActualPosition.y - transform.position.y;
-            Debug.LogError($"{positionX} e {positionY}");
-
-            var heading = _playerTransform.position - transform.position;
-            var direction = heading / heading.magnitude;
-
-            //Debug.DrawLine(transform.position, _playerTransform.position, Color.red);
-            Debug.DrawRay(transform.position, direction, Color.red);
             Debug.LogWarning(direction);
-            //VerifyMovimentAnimation(positionX, positionY, Speed);
+
+            VerifyMovimentAnimation(direction, Speed);
         }
 
     }
@@ -50,64 +55,63 @@ public class BelzebuController : MonoBehaviour
     private void OnCollisionEnter2D( Collision2D collision )
     {
         CanMove = false;
-
     }
 
-    private void VerifyMovimentAnimation(float positionX, float positionY, float speed)
+    private void VerifyMovimentAnimation(Vector2 direction, float speed)
     {
+        var xPosition = direction.x;
+        var yPosition = direction.y;
 
         if(speed > 0)
         {
-            //Right moviments
-            if (positionX > 0 && positionY > 0)
+
+            if (xPosition >= X_RIGHT && (yPosition >= Y_DOWN_TOLERANCE || yPosition <= Y_UP_TOLERANCE))
+                AnimationMovimentRight();
+
+            if (xPosition <= X_LEFT && (yPosition >= Y_DOWN_TOLERANCE || yPosition <= Y_UP_TOLERANCE))
+                AnimationMovimentLeft();
+
+            if (yPosition >= Y_UP && (xPosition >= X_LEFT_TOLERANCE || yPosition <= X_RIGHT_TOLERANCE))
+                AnimationMovimentUp();
+
+            if (yPosition <= Y_DOWN && (xPosition >= X_LEFT_TOLERANCE || yPosition <= X_RIGHT_TOLERANCE))
+                AnimationMovimentDown();
+
+            if(xPosition >= X_RIGHT_TOLERANCE && yPosition >= Y_UP_TOLERANCE)
                 AnimationMovimentRightUp();
 
-            if (positionX > 0 && positionY < 0)
+            if (xPosition >= X_RIGHT_TOLERANCE && yPosition <= Y_DOWN_TOLERANCE)
                 AnimationMovimentRightDown();
 
-            if (positionX > 0 && positionY == 0)
-                AnimationMovimentRight();
+            if (xPosition <= X_LEFT_TOLERANCE && yPosition >= Y_UP_TOLERANCE)
+                AnimationMovimentLeftUp();
+
+            if (xPosition <= X_LEFT_TOLERANCE && yPosition <= Y_DOWN_TOLERANCE)
+                AnimationMovimentLeftDown();
         }
     }
 
-    private void AnimationMovimentUp()
-    {
 
+    private void SetAnimatorParameters(float horizontal, float vertical)
+    {
+        _animator.SetFloat("Horizontal", horizontal);
+        _animator.SetFloat("Vertical", vertical);
     }
 
-    private void AnimationMovimentDown()
-    {
+    private void AnimationMovimentUp() => SetAnimatorParameters(0, 1);
 
-    }
+    private void AnimationMovimentDown() => SetAnimatorParameters(0, -1);
 
-    private void AnimationMovimentLeft()
-    {
+    private void AnimationMovimentLeft() => SetAnimatorParameters(-1, 0);
 
-    }
+    private void AnimationMovimentRight() => SetAnimatorParameters(1, 0);
 
-    private void AnimationMovimentRight()
-    {
+    private void AnimationMovimentRightUp() => SetAnimatorParameters(1, 1);
 
-    }
+    private void AnimationMovimentRightDown() => SetAnimatorParameters(1, -1);
 
-    private void AnimationMovimentRightUp()
-    {
+    private void AnimationMovimentLeftUp() => SetAnimatorParameters(-1, 1);
 
-    }
-
-    private void AnimationMovimentRightDown()
-    {
-
-    }
-
-    private void AnimationMovimentLeftUp()
-    {
-
-    }
-
-    private void AnimationMovimentoLeftDown()
-    {
-
-    }
+    private void AnimationMovimentLeftDown() => SetAnimatorParameters(-1, -1);
 
 }
